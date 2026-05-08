039d133ff016d1597c0f8f17afb3b2f6dd0abde2
vanitha-5 <vanithamani17006@gmail.com>
2026-05-08 13:27:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base:'./',
+    base:'',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
